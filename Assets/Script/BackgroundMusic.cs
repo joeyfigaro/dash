6d9ce32f61bacfbcbcbcbed9b1a00070e6c266ad
Joey Figaro <joey@joeyfigaro.com>
2014-07-13 13:07:11 -0400

    added new background music

---
+++ Assets/Script/BackgroundMusic.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusic : MonoBehaviour {
+	public GameObject music;
+
+	void Start () {
+		GameObject G = GameObject.FindGameObjectWithTag("GameController");	
+
+		if(!G) {
+			Instantiate(music, Vector3.zero, Quaternion.identity);
+		}
+	}
+}
